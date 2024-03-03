1f13ccdace23447a57ce472bbcb09237e4015afd
João Marques <100974516+jopepm@users.noreply.github.com>
2024-03-03 21:01:57 +0000

    Delete test_name.py

--- test_name.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome('path_to_chromedriver')
-driver.get('https://example.com')
-
-title = driver.title
-print("Title of the website:", title)
-
-driver.quit()
