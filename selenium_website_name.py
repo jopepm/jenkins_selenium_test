ba459406519a795e228a45163077afea3e258331
João Marques <100974516+jopepm@users.noreply.github.com>
2024-03-03 21:56:42 +0000

    Update selenium_website_name.py

--- selenium_website_name.py
+++ selenium_website_name.py
@@ -1,11 +1,16 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+html_file_path = os.path.join(script_dir, 'example.html')
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options = options)
-driver.get('./example.html')
+driver.get(html_file_path)
 
 title = driver.title
 print("Title of the website:", title)
